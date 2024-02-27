8fe61f6fdfdd1b9d978a1acf24fe03e7a390dca4
Zhuo <yi.zhuo@zilxap18.zi.local>
2024-02-27 16:36:21 +0100

    Mirko's function to change the title of the matlab window and the color of the command window to distinguish different matlab windows example: setDeskTitleCmdColor('ParDstructWrap',6)

--- helpers/setDeskTitleCmdColor.m
+++ helpers/setDeskTitleCmdColor.m
@@ -1,23 +1,48 @@
 function setDeskTitleCmdColor(Ttl,Clr)
+
 %% input:
-% - Ttl: title string 
-% - Clr: integer currently 1-6
+
+% - Ttl: title string
+
+% - Clr: integer (currently 1-6)
+
+ 
 
 %% set title
+
 mainFrame = com.mathworks.mde.desk.MLDesktop.getInstance.getMainFrame;
-mainFrame.setTitle("Matlab - " + string(Ttl))
+
+currTitle = string(mainFrame.getTitle);
+
+strsplit(currTitle, "     ////     ");
+
+mainFrame.setTitle(ans(1) + "     ////     " + string(Ttl))
+
+ 
 
 %% Change color of the command window
+
 % color options
-Clrs = [.99 .9 .9;...
-.9 .99 .9;...
-.9 .9 .99;...
-.99 .99 .9;...
- .99 .9 .99;...
-.6 .99 .99];
+
+Clrs = [.99 .8 .8;...
+
+.8 .99 .8;...
+
+.8 .8 .99;...
+
+.99 .99 .8;...
+
+.99 .8 .99;...
+
+.8 .99 .99];
+
+ 
 
 cmdWinDoc = com.mathworks.mde.cmdwin.CmdWinDocument.getInstance;
+
 listeners = cmdWinDoc.getDocumentListeners;
+
 jTextArea = listeners(5);  % 4 & 5
+
 jTextArea.setBackground(java.awt.Color(Clrs(Clr,1), Clrs(Clr,2), Clrs(Clr,3)));
 
