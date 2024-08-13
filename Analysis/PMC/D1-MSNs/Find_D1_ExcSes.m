a406dc063e9ff276802a2591ba4cf276d1eb7d9b
Angela Zhuo <58564403+AngelaZhuo@users.noreply.github.com>
2024-08-13 11:53:29 +0200

    modified mpsth.m
    Modified mpsth.m to run the tiledlayout on Find_D1_ExcSes.m

--- Analysis/PMC/D1-MSNs/Find_D1_ExcSes.m
+++ Analysis/PMC/D1-MSNs/Find_D1_ExcSes.m
@@ -22,8 +22,8 @@ for ux = 1:numel(uids)
     end
     
     %Create a big figure
-    BigFig = figure('Position', [100, 100, 1600, 1200]);  % Adjust the size as needed
-    t = tiledlayout(5, 4, 'TileSpacing', 'Compact', 'Padding', 'Compact');
+    BigFig = figure('Position', [10, 10, 2000, 1800]);  % Adjust the size as needed
+    t = tiledlayout(5, 8, 'TileSpacing', 'Compact', 'Padding', 'Compact');
     
     % Get the raster plot from pulse 1-10
     set(groot, 'DefaultFigureVisible',1)
@@ -32,32 +32,26 @@ for ux = 1:numel(uids)
         sham_pulse{px} = sham_pulses(px:10:end);
 %         ps(px) = figure;
         %Fill the BigFig with subplots
-        nexttile(t);
         for mx = 0:1
             if ~mx
                 pulse_time = sham_pulse{px};
-            [psth_sham{px}, trialspx_sham{px}, plotaxes_s] = mpsth_pj_x(spikes,pulse_time,'pre', 0, 'post',20 , 'binsz',...
-                1, 'tb', 1, 'chart', 2,'fr',0,...
-                'subplots',[2, 2,mx+3; 2, 2, mx+1]);
+            [psth_sham{px}, trialspx_sham{px}] = mpsth_az(spikes,pulse_time,'pre', 0, 'post',20 , 'binsz',...
+                1, 'tb', 1, 'chart', 2,'fr',0);
                 title('sham')
             else
                 pulse_time = pulse{px};
-            [psth{px}, trialspx{px}, plotaxes_l] = mpsth_pj_x(spikes,pulse_time,'pre', 0, 'post',20 , 'binsz',...
-                    1, 'tb', 1, 'chart', 2,'fr',0,...
-                    'subplots',[2, 2,mx+3; 2, 2, mx+1]);
+            [psth{px}, trialspx{px}] = mpsth_az(spikes,pulse_time,'pre', 0, 'post',20 , 'binsz',...
+                    1, 'tb', 1, 'chart', 2,'fr',0);
                 title('laser')
             end
         end
         % Add a text label above the subplot
-        annotation('textbox', 'Position', [0.5, 1.02, 0, 0] + [0, 0, 0, 0] + t.Children(end).Position, ...
-           'String', "Pulse " + string(px), 'Units', 'normalized', 'FontSize', 12, ...
-           'FontWeight', 'bold', 'Interpreter', 'none', 'HorizontalAlignment', 'center', ...
-           'EdgeColor', 'none');
-%         axes('Position', ps(px).Position.*[0 0 1 1], 'Box', 'off', 'Color', 'none', 'YColor', 'none', 'XColor', 'none');
-%         text(0.5,1.02, "Pulse " + string(px),'Units','centimeter','FontSize',12,'FontWeight','bold','Interpreter','none','HorizontalAlignment','center')
+        % axes('Position', t.Children(10).Position.*[0 0 1 1], 'Box', 'off', 'Color', 'none', 'YColor', 'none', 'XColor', 'none');
+        % text(0.5,1.02, "Pulse " + string(px),'Units','centimeter','FontSize',12,'FontWeight','bold','Interpreter','none','HorizontalAlignment','center')
 %         ps(px).Position = ps(px).Position + [0 0 0 0.5];
 %         linkaxes(ps(px).Children([3 5]))
-%         linkaxes(ps.Children([1 3]))
+        linkaxes(t.Children([2 4]))
+        sgtitle(t.Children(1:4),append('pulse', num2str(px)))
     end
 %     cf = univCombFig(ps,[2 5],1);
 end
